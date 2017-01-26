58efa3a005055bd23ff71708ef47bbd1d30323d0
rainiera <rainier@utexas.edu>
2017-01-26 16:16:45 -0600

    Change blog dir and remove git push -f

--- scripts/write_more.py
+++ scripts/write_more.py
@@ -4,7 +4,7 @@ import os
 import subprocess
 print os.getcwd()
 os.getcwd()
-os.chdir("/Users/rainierababao/cs/gamma/content")
+os.chdir("/Users/rainierababao/bog/gamma/content/")
 words = [arg for arg in sys.argv[1:]]
 with open("{}.md".format(('-').join(words)), "w") as beak:
     beak.write("Title: {}\n".format((' ').join(words)))
@@ -24,7 +24,7 @@ with open("{}.md".format(('-').join(words)), "w") as beak:
     beak.write("Title: {}\\n".format((' ').join(words)))
     beak.write("Date: {}\\n".format(time.strftime("%Y-%m-%d")))
     beak.write("Category: General\\n\\n")
-    beak.write("#### Also a terrible attempt at writing a quine\\n\\n")
+    beak.write("Automates writing test content with correct metadata for Pelican. This is the partial code for it, which can also be found [here](https://github.com/rainiera/dotfiles/blob/master/scripts/write_more.py):\n\n")
     # ...turtles all the way down :)
 ```
 """)
